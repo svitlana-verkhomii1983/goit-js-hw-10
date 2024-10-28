256be725ac3f289448205eff287de50039d583c6
svitlana-verkhomii1983 <svetik_hnu@ukr.net>
2024-10-28 19:04:06 +0000

    Deploying to gh-pages from @ svitlana-verkhomii1983/goit-js-hw-10@62cbb69346f18301e4666848bac761b3ad782004 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
 import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",o=>{o.preventDefault();const t=parseInt(s.delay.value),r=s.state.value;new Promise((e,l)=>{setTimeout(()=>{r==="fulfilled"?e(t):l(t)},t)}).then(e=>{i.success({title:"✅ Fulfilled",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"❌ Rejected",message:`Rejected promise in ${e}ms`,position:"topRight"})})});
-//# sourceMappingURL=2-promises.js.map
+//# sourceMappingURL=2-snackbar.js.map
